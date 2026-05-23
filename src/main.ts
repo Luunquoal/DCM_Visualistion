12a249942ca9f41059e81b17c5049745691af92e
Luunquoal <76056667+Luunquoal@users.noreply.github.com>
2026-05-23 06:11:03 +0200

    getting the module from @types

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as DCM from './My_DCM.js';
-import * as THREE from 'three/';
+import * as THREE from '../node_modules/@types/three/build/three.cjs';
 
 const img_elements : HTMLCollectionOf<HTMLImageElement> = document.getElementsByTagName("img");
 const input_elements : HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
